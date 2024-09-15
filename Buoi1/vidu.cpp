581e05d487093e35d7a1dacdd54bbe507f12df3a
Trần Quang Huy <92586415+Conan286@users.noreply.github.com>
2024-09-15 15:00:23 +0700

    Add files via upload

---
+++ Buoi1/vidu.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+ int soNguyen;
+ float soThuc;
+ char xauKyTu[101];
+ scanf("%d %f %s",&soNguyen,&soThuc,&xauKyTu);
+ printf("So nguyen vua nhap la: %d\n",soNguyen);
+ printf("So thuc vua nhap la: %f\n",soThuc);
+ printf("Xau Ky Tu vua nhap la: %s\n",xauKyTu);
+}
